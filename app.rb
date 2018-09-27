8844247089e3059eb870ebe8b1029b26b31d5037
Ayana Bando <ayanaflax@yahoo.com>
2018-09-27 01:51:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do 
+    "Welcome to your app!!!! I BUILT THIS!"
+  end
 
 end
